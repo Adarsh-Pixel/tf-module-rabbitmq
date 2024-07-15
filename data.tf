2479b2f2e104f58b1d9b8b2d8aa9f7f852cf242c
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-07-15 14:07:30 +0530

    Git Auto Push

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b55-terraform-state"
+    bucket = "b55-tf-state"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
